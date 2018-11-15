203ede8672b78f41cc33e809f27ac3426105df61
song <sojoho1122@gmail.com>
2018-11-15 12:50:07 +0900

    git save

---
+++ spring_board/src/main/java/com/spring/persistence/ReplyDAO.java
@@ -0,0 +1,18 @@
+package com.spring.persistence;
+
+import java.util.List;
+
+import com.spring.domain.Criteria;
+import com.spring.domain.ReplyVO;
+
+public interface ReplyDAO {
+	public int create(ReplyVO vo) throws Exception;
+	public int update(ReplyVO vo) throws Exception;
+	public int delete(int rno) throws Exception;
+	public ReplyVO get(int rno) throws Exception;
+	//public List<ReplyVO> list(int bno) throws Exception;	
+	public List<ReplyVO> listPage(int bno,Criteria cri) throws Exception;
+	public int count(int bno) throws Exception;
+	//댓글
+	public int getBno(int rno) throws Exception;
+}
